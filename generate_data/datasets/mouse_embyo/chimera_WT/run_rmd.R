02589426c19444a5bf6c6ccc8167a3d466883be0
Alsu Missarova <alsu@codon-login-05.ebi.ac.uk>
2023-04-28 11:43:28 +0100

    upd

--- generate_data/datasets/mouse_embyo/chimera_WT/run_rmd.R
+++ generate_data/datasets/mouse_embyo/chimera_WT/run_rmd.R
@@ -1,3 +1,3 @@
 library(rmarkdown)
 
-render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/datasets/mouse_embryo/chimera_WT/get_cimera_WT.Rmd")
+render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/datasets/mouse_embryo/chimera_WT/get_chimera_WT.Rmd")
