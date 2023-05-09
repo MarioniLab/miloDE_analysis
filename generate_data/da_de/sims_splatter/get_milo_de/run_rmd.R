4ad6ad8f0816a161e1db184d48f3a7a83bc46d95
amissarova <alsu.missarova.32@gmail.com>
2023-05-09 10:45:34 +0100

    get milo stat for da de

---
+++ generate_data/da_de/sims_splatter/get_milo_de/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/da_de/sims_splatter/get_milo_de/get_milo_de.Rmd")
---
+++ generate_data/da_de/sims_splatter/get_milo_nhoods/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/da_de/sims_splatter/get_milo_nhoods/get_milo_nhoods.Rmd")
