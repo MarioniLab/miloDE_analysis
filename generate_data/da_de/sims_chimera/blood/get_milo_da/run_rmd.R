778b4395001d74f878beadcac88e5c174683a30d
amissarova <alsu.missarova.32@gmail.com>
2023-05-16 10:21:15 +0100

    upd

---
+++ generate_data/da_de/sims_chimera/blood/get_milo_da/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/da_de/sims_chimera/blood/get_milo_da/get_milo_da.Rmd")
---
+++ generate_data/da_de/sims_chimera/blood/get_milo_da/sub_rmd.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#INITIALISE FOLDERS
+my_folder=/nfs/research/marioni/alsu
+out_folder=${my_folder}/clust_out/lung
+err_folder=${my_folder}/clust_err/lung
+
+#SELECT SCRIPT
+#If you change this, you MUST update the wrapper's grep
+script_name=miloDA_da_de
+
+#CHOOSE PARAMETERS
+#RAM in megabytes
+memory=600000
+r_command="rusage[mem=${memory}]"
+#num_processors
+nproc=20
+
+
+smg=/hps/software/users/marioni/alsu/singularity/alsu_miloDE.simg
+script=/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/da_de/sims_chimera/blood/get_milo_da/run_rmd.R
+
+bsub -q bigmem -e ${err_folder}/${script_name} \
+-o ${out_folder}/${script_name} \
+-M $memory -R $r_command -n $nproc -J ${script_name} \
+"singularity exec $smg Rscript $script"
