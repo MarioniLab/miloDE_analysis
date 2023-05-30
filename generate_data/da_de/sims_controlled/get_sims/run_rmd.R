295935d37b089a66ffe7b0e55628a2ba5d7b5597
amissarova <alsu.missarova.32@gmail.com>
2023-05-30 13:02:14 +0100

    do diff sims'

---
+++ generate_data/da_de/sims_controlled/get_sims/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/da_de/sims_controlled/get_sims/get_sims_splatter.Rmd")
---
+++ generate_data/da_de/sims_controlled/get_stat/run_rmd.R
@@ -0,0 +1,3 @@
+library(rmarkdown)
+
+render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/da_de/sims_controlled/get_stat/get_stat.Rmd")
