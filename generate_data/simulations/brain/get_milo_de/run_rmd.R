f964ff5345849b944fa6dea95ba80973ed1c17b9
Alsu Missarova <alsu@codon-login-04.ebi.ac.uk>
2024-03-16 18:40:46 +0000

    delete old and unused scripts

--- generate_data/simulations/brain/get_milo_de/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/simulations/brain/get_milo_de/get_milo_de.Rmd")
--- generate_data/simulations/cardiomyocytes/simulation_counts/run_rmd.R
+++
@@ -1,3 +0,0 @@
-library(rmarkdown)
-
-render("/nfs/research/marioni/alsu/hubmap_metaRef/miloDE_analysis/generate_data/simulations/cardiomyocytes/simulation_counts/simulation_counts.Rmd")
